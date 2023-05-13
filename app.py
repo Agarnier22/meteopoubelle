99a5646e4c6d7d4b4d8739e82ed127b9069fdba1
elsiehoffet-94 <ehoffet@gmail.com>
2023-05-13 03:38:13 +0200

    add cacl for piechart

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import pydeck as pdk
+import matplotlib.pyplot as plt
 
-'# ☀️ 🗑 Meteopoubelle 🌊'
+
+'# ☀️ 🗑 Météopoubelle 🌊'
 st.markdown('''
 Découvrez les zones d'accumulation de déchets diffus à Marseille !  
 Data source: [Mer-Terre](https://www.zero-dechet-sauvage.org/ressources)
 ''')
 
-st.header('Explorez la zone')
 
 if 'params' not in st.session_state:
     st.session_state.df = pd.read_csv('coords_gps')
@@ -26,5 +28,28 @@ wind_choice = st.sidebar.selectbox('', wind)
 season_choice = st.sidebar.selectbox('', season)
 rain_choice = st.sidebar.selectbox('', rain)
 
-st.map(df)
+#st.map(df)
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=43.2,
+        longitude=5.35,
+        zoom=11
+      #  pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=df,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=['POIDS_TOTAL'],
+        ),
+    ],
+))
 
+
+# Pie chart, where the slices will be ordered and plotted counter-clockwise:
+#pie_chart = pd.read_csv('piechart')
+#fig = px.pie(pie_chart, values='vals',  title='Population of European continent')
+#st.pyplot(pie_chart)
